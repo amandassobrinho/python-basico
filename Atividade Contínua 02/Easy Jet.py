a533dbec84540fd8d2f025255ea81c1eddd7b4f1
amandassobrinho <68513559+amandassobrinho@users.noreply.github.com>
2020-07-19 15:50:35 -0300

    Create Easy Jet.py

---
+++ Atividade Contínua 02/Easy Jet.py
@@ -0,0 +1,7 @@
+largura= float(input())
+comprimento= float(input())
+altura= float(input())
+if largura<=45 and comprimento<=56 and altura<=25:
+    print("PERMITIDA")
+else:
+    print("PROIBIDA")
