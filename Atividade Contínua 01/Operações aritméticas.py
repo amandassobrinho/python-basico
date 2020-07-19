4f91571dd6f344aa44e132adeadfaeab761de644
amandassobrinho <68513559+amandassobrinho@users.noreply.github.com>
2020-07-19 15:45:34 -0300

    Create Operações aritméticas.py

---
+++ Atividade Contínua 01/Operações aritméticas.py
@@ -0,0 +1,22 @@
+n = float(input())
+import math
+
+print ( "i)",n * n ) 
+
+print ("ii)",n ** math.exp ( 1)) 
+
+print ("iii)", n ** 0.5) 
+
+print ("iv)" ,n ** (1/3)) 
+
+print ("v)", n ** (1/n)) 
+
+print ("vi)", math.exp (1) * n ) 
+
+print ("vii)", math.pi / n ) 
+
+print ("viii)", math.log(n,7))
+
+print ("ix)", math.log (n,math.exp(1)))
+
+print ("x)", math.log ( n,math.pi))
